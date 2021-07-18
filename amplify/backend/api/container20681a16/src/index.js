f6108f7ba03007d0943cd044ca28c7c4716ff9d5
Hayato Watanabe <solideye@me.com>
2021-07-18 13:18:59 +0900

    remove body-parser package

--- amplify/backend/api/container20681a16/src/index.js
+++ amplify/backend/api/container20681a16/src/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 3001;
 
 const {
@@ -9,8 +8,8 @@ const {
 } = require('./DynamoDBActions');
 
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Enable CORS for all methods
 app.use(function (req, res, next) {
