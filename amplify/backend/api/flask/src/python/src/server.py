e479359b18bb37a9fe9bbde91e5f4279dda67ea9
Hayato Watanabe <solideye@me.com>
2021-07-18 14:07:10 +0900

    update flask app logic

--- amplify/backend/api/flask/src/python/src/server.py
+++ amplify/backend/api/flask/src/python/src/server.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from random import randrange
+
 server = Flask(__name__)
 
-@server.route('/images')
+@server.route('/random')
 def hello():
-    return 'Processing images...'
+    return str(randrange(100))
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
+   
